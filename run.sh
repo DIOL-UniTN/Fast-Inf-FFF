101c5a4086aa5e417227b8da5320be5fcef4a6a4
Leonardo Lucio Custode <leocus4@gmail.com>
2023-09-29 19:18:28 +0200

    Add remote experiment tracking

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 
+
+# Get dagshub user and pass from environment variables
+dagshub_user=$DAGSHUB_USER
+dagshub_pass=$DAGSHUB_PASS
+
+dagshub_tracking_string="MLFLOW_TRACKING_URI=https://dagshub.com/leocus4/TinyFFF.mlflow \
+MLFLOW_TRACKING_USERNAME=$dagshub_user \
+MLFLOW_TRACKING_PASSWORD=$dagshub_pass "
+
+
 # Check whether an image called "tinyfff" exists
 exists=`docker ps -a | grep tinyfff | wc -l`;
 echo "Image exists: $exists";
@@ -15,6 +25,6 @@ docker run --rm -dit \
     -v $(pwd):/home/user/tinyfff tinyfff
 
 # Exec the program
-docker exec -it tinyfff_container sh -c "cd /home/user/tinyfff && python fff_experiment_mnist.py $1 $2 $3"
+docker exec -it tinyfff_container sh -c "cd /home/user/tinyfff && $dagshub_tracking_string python fff_experiment_mnist.py $1 $2 $3"
 
 docker kill tinyfff_container
